17689a0a91f5a9d8cd499eab6742d1ef95398413
= <hhanif2@masonlive.gmu.edu>
2018-11-24 02:37:42 -0500

    Few more activities done

--- app/src/main/java/com/example/hammadhanif/cs_477_final_project/PhotoActivity.java
+++ app/src/main/java/com/example/hammadhanif/cs_477_final_project/PhotoActivity.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class QualificationInfoActivity extends AppCompatActivity {
+public class PhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qualification_info);
+        setContentView(R.layout.activity_photo);
     }
 
-    public void onClickContinue(View view) {
-        Intent continueIntent = new Intent(this, PhotoActivity.class);
-        startActivity(continueIntent);
+    public void onClickQualification(View view) {
+        Intent backIntent = new Intent(this, QualificationInfoActivity.class);
+        startActivity(backIntent);
     }
 }
