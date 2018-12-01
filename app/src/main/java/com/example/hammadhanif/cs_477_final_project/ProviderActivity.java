187278859c886b6eef9097803d0d9b1e841bb42f
= <hhanif2@masonlive.gmu.edu>
2018-12-01 01:12:09 -0500

    Merge branch 'mansour' of github.com:hh28928/CS477FinalProject
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/example/hammadhanif/cs_477_final_project/ProviderActivity.java
+++ app/src/main/java/com/example/hammadhanif/cs_477_final_project/ProviderActivity.java
@@ -1,21 +1,118 @@
 package com.example.hammadhanif.cs_477_final_project;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class ProviderActivity extends AppCompatActivity {
 
+    EditText code;
+    String number;
+
+    private String verificationid;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider);
+
+        code = findViewById(R.id.digits_entered);
+        progressBar = findViewById(R.id.progressbar);
+        mAuth = FirebaseAuth.getInstance();
+
+        number = getIntent().getExtras().getString("PHONE");
+        sendVerificationCode(number);
     }
 
     public void onClickContinueMap(View view) {
-        Intent continueIntent = new Intent(this,CurrentLocationMap.class);
-        startActivity(continueIntent);
+        String code_input = code.getText().toString().trim();
 
+        if ((code_input.isEmpty() || code.length() < 6)){
+
+            code.setError("Enter code...");
+            code.requestFocus();
+            return;
+        }
+        verifyCode(code_input);
     }
+
+    private void verifyCode(String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationid, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(ProviderActivity.this, CurrentLocationMap.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(ProviderActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+    }
+
+    private void sendVerificationCode(String number){
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallBack
+        );
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken)
+        {
+            super.onCodeSent(s, forceResendingToken);
+            verificationid = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null){
+                progressBar.setVisibility(View.VISIBLE);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(ProviderActivity.this, e.getMessage(),Toast.LENGTH_LONG).show();
+
+        }
+    };
 }
--- app/src/main/java/com/example/hammadhanif/cs_477_final_project/SigninActivity.java
+++ app/src/main/java/com/example/hammadhanif/cs_477_final_project/SigninActivity.java
@@ -1,23 +1,43 @@
 package com.example.hammadhanif.cs_477_final_project;
 
 import android.content.Intent;
+import android.hardware.camera2.TotalCaptureResult;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SigninActivity extends AppCompatActivity {
+
+    EditText phone_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-
+        phone_et = findViewById(R.id.phone_number);
     }
 
     public void onClickProvider(View view) {
-        Intent providerIntent = new Intent(this, ProviderActivity.class);
-        startActivity(providerIntent);
+        if (phone_et.getText().toString().equals("")) {
+            Toast.makeText(this, "Phone Number Not Entered!", Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            String phone = phone_et.getText().toString();
+            Intent providerIntent = new Intent(this, ProviderActivity.class);
+            providerIntent.putExtra("PHONE", phone);
+            startActivity(providerIntent);
+        }
     }
 
     public void OnclickUser(View view) {
